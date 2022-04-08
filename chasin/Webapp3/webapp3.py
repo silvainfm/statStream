b9256c90ea310f0c143dac918774a70866aebdeb
François-Marie Brych <61249160+silvainfm@users.noreply.github.com>
2022-04-07 19:14:32 -0500

    updated states

--- chasin/Webapp3/webapp3.py
+++ chasin/Webapp3/webapp3.py
@@ -56,11 +56,7 @@ if authentication_status:
 
     state = st.sidebar.multiselect('Select the State:',
         options=dfshow['State'].unique(),
-        default=['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
-           'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
-           'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
-           'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
-           'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY', 'Canada', 'Outside the US'] )
+        default=dfshow['State'].unique() )
 
 # maybe add which score as a selection first and then show 
 # start with a question like which score do you wan to start filtering with 
