061577b2242580d074a18b8a9df22a218594aa8d
François-Marie Brych <61249160+silvainfm@users.noreply.github.com>
2022-04-18 21:22:40 -0500

    Update webapp3.py

--- chasin/Webapp3/webapp3.py
+++ chasin/Webapp3/webapp3.py
@@ -89,8 +89,6 @@ if authentication_status:
     # CSV Download button 
     st.download_button(label = 'Export current selection to CSV', data = df1_selection.to_csv(), mime='text/csv')
 
-    # https://discuss.streamlit.io/t/how-to-take-text-input-from-a-user/187/3
-
     pdf_col_ucaas = ['Industry Sector - Job Title', 
      'Operating System', 
      'Cloud Service Provider', 
@@ -104,94 +102,9 @@ if authentication_status:
      'communication',
      'network',
      'consulting',
-     'IT',
-     'Communication Systems Solutions - Business SMS and MMS',
-     'Communication Systems Solutions - Cloud based phone systems (VOIP)',
-     'Communication Systems Solutions - Contact Center Optimization',
-     'Communication Systems Solutions - Contract Management',
-     'Communication Systems Solutions - Contract Negotiations',
-     'Communication Systems Solutions - Hosted PBX',
-     'Communication Systems Solutions - Outsourced Consulting',
-     'Communication Systems Solutions - Telecom Expense Management',
-     'Communication Systems Solutions - Unified Communications',
-     'Consulting / Outsourcing Solutions - Call Centers',
-     'Consulting / Outsourcing Solutions - Digital Worker',
-     'Consulting / Outsourcing Solutions - Vendor Evaluations',
-     'Cybersecurity Solutions - Application Security - Vulnerability Assessment',
-     'Cybersecurity Solutions - Application Security and WAF',
-     'Cybersecurity Solutions - Cloud Security',
-     'Cybersecurity Solutions - Critical Event Management',
-     'Cybersecurity Solutions - Security Awareness Computer-Based Training for end-users',
-     'Cybersecurity Solutions - Data Loss Protection',
-     'Cybersecurity Solutions - Data Center Security',
-     'Cybersecurity Solutions - Data Security',
-     'Cybersecurity Solutions - DevOps Security',
-     'Cybersecurity Solutions - Disaster / Backup Recovery',
-     'Cybersecurity Solutions - Email Security',
-     'Cybersecurity Solutions - Employee monitoring - Insider Threat Detection',
-     'Cybersecurity Solutions - Encryption',
-     'Cybersecurity Solutions - Endpoint detection & response (EDR)',
-     'Cybersecurity Solutions - Extended Detection and Response (XDR)',
-     'Cybersecurity Solutions - Endpoint prevention',
-     'Cybersecurity Solutions - Fraud Prevention / Transaction Security',
-     'Cybersecurity Solutions - Identity & Access Management',
-     'Cybersecurity Solutions - Multi-factor authentication',
-     'Cybersecurity Solutions - Zerotrust',
-     'Cybersecurity Solutions - Industrial / IoT Security',
-     'Cybersecurity Solutions - Managed Security Service Provider',
-     'Cybersecurity Solutions - Mobile App Security',
-     'Cybersecurity Solutions - Mobile Security',
-     'Cybersecurity Solutions - Network Security - Firewall',
-     'Cybersecurity Solutions - Network Security - Intrusion Prevention Systems',
-     'Cybersecurity Solutions - Network Security - Monitoring & forensics',
-     'Cybersecurity Solutions - Network Security - Unified Threat Management',
-     'Cybersecurity Solutions - Outsourced Consulting',
-     'Cybersecurity Solutions - Penetration Testing and Simulation',
-     'Cybersecurity Solutions - Phishing',
-     'Cybersecurity Solutions - Physical Security - Surveillance and Access Control',
-     'Cybersecurity Solutions - Privileged Access Management',
-     'Cybersecurity Solutions - Ransomware',
-     'Cybersecurity Solutions - Risk & Compliance',
-     'Cybersecurity Solutions - Security Incident Response',
-     'Cybersecurity Solutions - Security Operations Center - SOC as a Service',
-     'Cybersecurity Solutions - Security Rating / Benchmarking',
-     'Cybersecurity Solutions - SIEM',
-     'Cybersecurity Solutions - SOAR - Security Orchestration Automation and Response',
-     'Cybersecurity Solutions - Third Party Cyber Risk Management',
-     'Cybersecurity Solutions - Threat Intelligence',
-     'Cybersecurity Solutions - User behavior monitoring',
-     'Cybersecurity Solutions - Web Security',
-     'Network Systems Solutions  - MPLS',
-     'Network Systems Solutions  - SD-WAN',
-     'Network Systems Solutions  - WAN',
-     'Consulting / Outsourcing Solutions - APIs',
-     'Consulting / Outsourcing Solutions - Cloud Enablement',
-     'Consulting / Outsourcing Solutions - Cloud Workload and Migration',
-     'Consulting / Outsourcing Solutions - Cybersecurity Services',
-     'Consulting / Outsourcing Solutions - IT Service Management (ITSM)',
-     'Consulting / Outsourcing Solutions - Technology Lifecycle Management',
-     'Consulting / Outsourcing Solutions - Vendor Evaluations']
-
-    # To PDF function
-    def to_pdf(df, company, pdf_list):
-        # loc the row of the company we want
-        pdf1 = df.loc[df['Company'] == company]
-        # pdf1 = pdf1.set_index('Company Name - Website - State')
-        pdf1 = pdf1[pdf_list]
-
-        # transpose df
-        pdf1 = pdf1.T
-
-        # convert to html to then convert to pdf
-        result = pdf1.to_html(f'{company}_ht.html', render_links = True) # look deeper into pandas tohtml to change the aspect 
-        pdf_name = f'{company}_report.pdf'
-        pdf.from_file(Path(__file__).parents[1], pdf_name)
-        return result
+     'IT']
     
-    df1_selection = df1_selection.set_index('Company Name - Website - State')
     # figure out if we want the user to be able to select the companies individually or just from the selection
-    # add a yes or no line for multiple or only a single company
-    # add a multiple choice between the categories for ucaas and all... 
     #company_bull = st.radio('Do you want to transfer the current selection to pdf or just one company?', ('Current Selection', '1 Company'))
 
     #if company_bull == 'Current Selection':
