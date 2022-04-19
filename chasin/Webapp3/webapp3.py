00332e8e21e6e04b30dfc4a96f3b7f2059cad01f
François-Marie Brych <61249160+silvainfm@users.noreply.github.com>
2022-04-19 11:26:35 -0500

    Update webapp3.py

--- chasin/Webapp3/webapp3.py
+++ chasin/Webapp3/webapp3.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pdfkit as pdf
 import streamlit_authenticator as stauth
 from pathlib import Path
+from docxtpl import DocxTemplate
 
 # be able to select each company
 # figure out the score filtering 
